4cb6e303ebcd9f13b8b244d7cd65476f2b1b458c
zxb <v-xubozhao@microsoft.com>
2025-11-12 12:31:02 +0800

    .

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, jsonify
+import requests
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    data = np.array([1, 2, 3])
+    df = pd.DataFrame(data, columns=["numbers"])
+    return jsonify({"sum": int(df["numbers"].sum())})
+
+if __name__ == "__main__":
+    app.run(debug=True)
